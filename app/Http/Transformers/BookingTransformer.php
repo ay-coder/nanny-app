daee47d0e3530ee99675d734ea0a81789b1fe377
Anuj Jaha <er.anujjaha@gmail.com>
2018-07-27 10:01:00 +0530

    Booking - Convertedt to Object

--- app/Http/Transformers/BookingTransformer.php
+++ app/Http/Transformers/BookingTransformer.php
@@ -24,7 +24,7 @@ class BookingTransformer extends Transformer
         $sitter         = (object) $item->sitter;
         $baby           = (object) $item->baby;
         $payment        = (object) $item->payment;
-        $paymentData    = [];
+        $paymentData    = (object) [];
 
         if(isset($payment) && isset($payment->id))
         {
