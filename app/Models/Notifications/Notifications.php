c0283a8d77e643d2c3869bb167f2e67202608108
Anuj Jaha <er.anujjaha@gmail.com>
2018-08-02 09:35:51 +0530

    Save notifiation - Fixed - added param in modal

--- app/Models/Notifications/Notifications.php
+++ app/Models/Notifications/Notifications.php
@@ -24,7 +24,7 @@ class Notifications extends BaseModel
      *
      */
     protected $fillable = [
-        "id", "user_id", "sitter_id", "icon", "description", "status", "is_read", "read_time", "created_at", "updated_at", 
+        "id", "user_id", "sitter_id", "booking_id", "icon", "description", "status", "is_read", "read_time", "created_at", "updated_at", 
     ];
 
     /**
