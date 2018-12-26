d67f0343950a767560611a717595244607669154
Anuj Jaha <er.anujjaha@gmail.com>
2018-12-26 21:18:02 +0530

    Editable button

--- app/Models/Sitters/Traits/Attribute/Attribute.php
+++ app/Models/Sitters/Traits/Attribute/Attribute.php
@@ -49,10 +49,11 @@ trait Attribute
      */
     public function getAdminActionButtonsAttribute()
     {
-        return 'N/A';
         $repository = new EloquentSittersRepository;
         $routes     = $repository->getModuleRoutes();
         
-        return $this->getEditButtonAttribute($routes, $repository->adminRoutePrefix, true) .  $this->getDeleteButtonAttribute($routes, $repository->adminRoutePrefix);
+        return $this->getEditButtonAttribute($routes, $repository->adminRoutePrefix, true);
+
+        // .  $this->getDeleteButtonAttribute($routes, $repository->adminRoutePrefix);
     }
 }
