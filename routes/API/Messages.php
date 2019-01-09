029804d26b6d86466f828949613013b7acd864d0
Anuj Jaha <er.anujjaha@gmail.com>
2019-01-09 10:56:05 +0530

    create message

--- routes/API/Messages.php
+++ routes/API/Messages.php
@@ -5,7 +5,7 @@ Route::group(['namespace' => 'Api'], function()
     
     Route::get('my-messages', 'APIMessagesController@myMessages')->name('messages.my-message');
     
-    Route::get('my-messages/create', 'APIMessagesController@createMyMessage')->name('messages.my-message');
+    Route::post('my-messages/create', 'APIMessagesController@createMyMessage')->name('messages.my-message');
 
     Route::post('messages/get-chat', 'APIMessagesController@getChat')->name('messages.get-chat');
 
